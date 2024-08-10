eaeb350b6499b8d19cbf5796b18057806f40c2d2
pyoneerC <maxcomperatutti@gmail.com>
2024-08-09 23:32:35 -0300

    bilingual more about me button

--- src/app/(with-footer)/(home)/_sections/AboutSection/AboutSection.tsx
+++ src/app/(with-footer)/(home)/_sections/AboutSection/AboutSection.tsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
 						{t("d2")}
 					</p>
 					<Link className={styles.goFAQLink} href="/faq">
-						Mas sobre mí&nbsp;
+						{t("mab")}&nbsp;
 						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
 								 stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
 								 className="icon icon-tabler icons-tabler-outline icon-tabler-info-square-rounded">
