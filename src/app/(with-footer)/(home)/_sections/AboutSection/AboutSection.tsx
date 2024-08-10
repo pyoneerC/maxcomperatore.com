ddba7fc01a858649cb283207841b02fe6e4bce66
pyoneerC <maxcomperatutti@gmail.com>
2024-08-10 17:10:52 -0300

    more about me button

--- src/app/(with-footer)/(home)/_sections/AboutSection/AboutSection.tsx
+++ src/app/(with-footer)/(home)/_sections/AboutSection/AboutSection.tsx
@@ -51,7 +51,8 @@ export const AboutSection = () => {
 						{t("mab")}&nbsp;
 						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
 								 stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-								 className="icon icon-tabler icons-tabler-outline icon-tabler-info-square-rounded">
+								 className="icon icon-tabler icons-tabler-outline icon-tabler-info-square-rounded"
+								 style={{display: "inline-block", verticalAlign: "text-bottom"}}>
 							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 							<path d="M12 9h.01" />
 							<path d="M11 12h1v4h1" />
