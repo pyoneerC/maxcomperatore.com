530bce102aead26460273d2183a81f9f0b9e1ad9
pyoneerC <maxcomperatutti@gmail.com>
2024-08-23 14:56:36 -0300

    translated first blog post

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,17 +1,19 @@
 import type { Post } from "~/types"
 import NextLink from "next/link"
 import styles from "./PostCard.module.css"
+import {useTranslations} from "next-intl"
 
 interface Props extends Post {}
 
 export const PostCard: React.FC<Props> = ({ slug, title, description, emoji }) => {
 	const href = `/blog/${slug}`
+	const t = useTranslations("Blog")
 
 	return (
 		<article className={styles.card}>
 			<NextLink className={styles.linkWrapper} href={href}></NextLink>
 			<div className={styles.infoWrapper}>
-				<h3 className={styles.title}>{title}</h3>
+				<h3 className={styles.title}>{t(title)}</h3>
 				<p>{description}</p>
 			</div>
 			<span className={styles.emoji}>{emoji}</span>
