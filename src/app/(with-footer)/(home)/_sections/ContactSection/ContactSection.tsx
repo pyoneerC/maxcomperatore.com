d4748ab89e15906ffe9bc9ed741472e5e95e01f2
pyoneerC <maxcomperatutti@gmail.com>
2024-08-30 13:05:52 -0300

    feat: icons to services section

--- src/app/(with-footer)/(home)/_sections/ContactSection/ContactSection.tsx
+++ src/app/(with-footer)/(home)/_sections/ContactSection/ContactSection.tsx
@@ -16,21 +16,50 @@ export const ContactSection = () => {
 			</p>
 			<div className={styles.servicesContainer}>
 				<div className={styles.serviceCard}>
-					<div className={styles.serviceCategory}>{t("servicesDigitalMarketing")}</div>
+					<div className={styles.serviceCategory}>{t("servicesDigitalMarketing")}
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+								 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+								 className="icon icon-tabler icons-tabler-outline icon-tabler-brush">
+							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+							<path d="M3 21v-4a4 4 0 1 1 4 4h-4" />
+							<path d="M21 3a16 16 0 0 0 -12.8 10.2" />
+							<path d="M21 3a16 16 0 0 1 -10.2 12.8" />
+							<path d="M10.6 9a9 9 0 0 1 4.4 4.4" />
+						</svg>
+					</div>
 					<ul>
 						<li>{t("servicesContentCreation")}</li>
 						<li>{t("servicesSEO")}</li>
 					</ul>
 				</div>
 				<div className={styles.serviceCard}>
-					<div className={styles.serviceCategory}>{t("servicesDevelopment")}</div>
+					<div className={styles.serviceCategory}>{t("servicesDevelopment")}
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+								 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+								 style={{ verticalAlign: "text-bottom" }}>
+							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+							<path d="M10 14l-2 -2l2 -2" />
+							<path d="M14 10l2 2l-2 2" />
+							<path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+						</svg>
+					</div>
 					<ul>
 						<li>{t("gameDevelopment")}</li>
 						<li>{t("backendDevelopment")}</li>
 					</ul>
 				</div>
 				<div className={styles.serviceCard}>
-					<div className={styles.serviceCategory}>{t("servicesOther")}</div>
+					<div className={styles.serviceCategory}>{t("servicesOther")}
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+								 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+								 className="icon icon-tabler icons-tabler-outline icon-tabler-keyframes">
+							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+							<path
+								d="M9.225 18.412a1.595 1.595 0 0 1 -1.225 .588c-.468 0 -.914 -.214 -1.225 -.588l-4.361 -5.248a1.844 1.844 0 0 1 0 -2.328l4.361 -5.248a1.595 1.595 0 0 1 1.225 -.588c.468 0 .914 .214 1.225 .588l4.361 5.248a1.844 1.844 0 0 1 0 2.328l-4.361 5.248z" />
+							<path d="M17 5l4.586 5.836a1.844 1.844 0 0 1 0 2.328l-4.586 5.836" />
+							<path d="M13 5l4.586 5.836a1.844 1.844 0 0 1 0 2.328l-4.586 5.836" />
+						</svg>
+					</div>
 					<ul>
 						<li>{t("servicesInformationalInterviews")}</li>
 						<li>{t("servicesTranslation")}</li>
