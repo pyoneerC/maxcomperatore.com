fc1395e23ac4850f739fe4dec4ff800c70ba2ffe
pyoneerC <maxcomperatutti@gmail.com>
2024-08-30 19:37:39 -0300

    feat: blog redirect icons
    thanks 4 the suggestion @sanclemente

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,7 +1,8 @@
 import type { Post } from "~/types"
 import NextLink from "next/link"
 import styles from "./PostCard.module.css"
-import {useTranslations} from "next-intl"
+import { useTranslations } from "next-intl"
+import { ArrowUpRight } from "~/components/Svg/ArrowUpRight"
 
 interface Props extends Post {}
 
@@ -12,7 +13,10 @@ export const PostCard: React.FC<Props> = ({ slug, title, description, emoji }) =
 		<article className={styles.card}>
 			<NextLink className={styles.linkWrapper} href={slug}></NextLink>
 			<div className={styles.infoWrapper}>
-				<h3 className={styles.title}>{t(title)}</h3>
+				<h3 className={styles.title}>
+					{t(title)}
+					<ArrowUpRight className={styles.icon} />
+				</h3>
 				<p>{t(description)}</p>
 			</div>
 			<span className={styles.emoji}>{emoji}</span>
