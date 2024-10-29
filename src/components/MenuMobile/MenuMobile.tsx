ea13c5c838772bff0dc5b37986a234dcd650bbb3
pyoneerC <maxcomperatutti@gmail.com>
2024-10-28 23:43:01 -0300

    translate menu mobile

--- src/components/MenuMobile/MenuMobile.tsx
+++ src/components/MenuMobile/MenuMobile.tsx
@@ -72,7 +72,7 @@ export const MenuMobile = () => {
 							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 							<path d="M7 4a2 2 0 0 0 -2 2v3a2 3 0 0 1 -2 3a2 3 0 0 1 2 3v3a2 2 0 0 0 2 2" />
 						</svg>
-						<span className={styles.themeText}>Idioma </span>
+						<span className={styles.themeText}>{t("language")} </span>
 						<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none"
 								 stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
 								 className="icon icon-tabler icons-tabler-outline icon-tabler-protocol" style={{ verticalAlign: 'text-bottom' }}>
@@ -95,7 +95,7 @@ export const MenuMobile = () => {
 							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 							<path d="M7 4a2 2 0 0 0 -2 2v3a2 3 0 0 1 -2 3a2 3 0 0 1 2 3v3a2 2 0 0 0 2 2" />
 						</svg>
-						<span className={styles.themeText2}>Tema </span>
+						<span className={styles.themeText2}>{t("theme")} </span>
 						<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none"
 								 stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
 								 className="icon icon-tabler icons-tabler-outline icon-tabler-protocol" style={{ verticalAlign: 'text-bottom' }}>
