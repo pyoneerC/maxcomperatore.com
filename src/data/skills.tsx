55afb1f5aa0445aab58524bac64f47a88e7bc35a
pyoneerC <maxcomperatutti@gmail.com>
2024-11-08 11:51:57 -0300

    gen x tooltips

--- src/data/skills.tsx
+++ src/data/skills.tsx
@@ -13,62 +13,49 @@ export const skills = [
 		name: "Python",
 		icon: <PythonLogo />,
 		brandColor: "#FFD445",
+		tooltip: "A versatile language for data analysis, automation, and web apps.",
 	},
 	{
 		name: "Docker",
 		icon: <DockerLogo />,
 		brandColor: "#099CEC",
+		tooltip: "Deploys apps in isolated containers for consistent performance.",
 	},
 	{
 		name: "Azure",
 		icon: <RedisLogo />,
 		brandColor: "#34BDEE",
+		tooltip: "Microsoft’s cloud for scalable apps, AI, and data solutions.",
 	},
 	{
 		name: "PostgreSQL",
 		icon: <PostgreSqlLogo />,
 		brandColor: "#336791",
+		tooltip: "Powerful database for managing complex data securely and reliably.",
 	},
-	//{
-	//	name: "MySQL",
-	//	icon: <MySQLLogo />,
-	//	brandColor: "#00758F",
-	//},
-	//{
-	//	name: "Rust",
-	//	icon: <RustLogo />,
-	//	brandColor: "#7A4929",
-	//},
-	// .NET related skills
-	//{
-	//	name: "DotNet",
-	//	icon: <DotNetLogo />,
-	//	brandColor: "#5027D5",
-	//},
-	//{
-	//	name: "Azure",
-	//	icon: <AzureLogo />,
-	//	brandColor: "#007FFF",
-	//},
 	{
 		name: "DotNet",
 		icon: <CsharpLogo />,
 		brandColor: "#512BD4",
+		tooltip: "Framework for building scalable, enterprise-grade applications.",
 	},
 	// Game development skills
 	{
 		name: "Unity",
 		icon: <UnityLogo />,
 		brandColor: "#818181",
+		tooltip: "Platform for developing interactive 2D and 3D gaming experiences.",
 	},
 	{
 		name: "Unreal",
 		icon: <UnrealLogo />,
 		brandColor: "#818181",
+		tooltip: "Engine for high-quality, immersive 3D worlds and game environments.",
 	},
 	{
 		name: "React",
 		icon: <CppLogo />,
 		brandColor: "#61DAFB",
+		tooltip: "Library for creating fast, interactive user interfaces for web apps.",
 	},
-]
+];
