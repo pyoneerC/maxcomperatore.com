4ce723e26c276b490abf1f63a2312ffdd250fe6a
pyoneerC <maxcomperatutti@gmail.com>
2024-12-19 20:16:27 -0300

    better arrow up

--- src/components/ScrollToTopButton/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton/ScrollToTopButton.tsx
@@ -30,7 +30,13 @@ const ScrollToTopButton: React.FC = () => {
 
 	return (
 		<div className={`${styles.scrollToTop} ${isVisible ? styles.show : ''}`} onClick={scrollToTop}>
-			↑
+			<svg className='bee bee-icons' width='24' height='24' viewBox='0 0 24 24' fill='none'
+					 xmlns='http://www.w3.org/2000/svg'>
+				<rect width='24' height='24' fill='none' />
+				<path
+					d='M11.2191 7.97609C11.6195 7.47568 12.3805 7.47568 12.7809 7.97609L18.7002 15.3753C19.2241 16.0301 18.7579 17 17.9194 17H6.08062C5.24212 17 4.77595 16.0301 5.29976 15.3753L11.2191 7.97609Z'
+					stroke='currentColor' stroke-width='2' />
+			</svg>
 		</div>
 	)
 }
