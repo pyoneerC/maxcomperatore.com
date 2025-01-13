673c57df2598f2976ba69478a4069d9f24c8387c
pyoneerC <maxcomperatutti@gmail.com>
2025-01-13 19:39:08 -0300

    try fix

--- src/app/(with-footer)/(home)/_sections/HeroSection/HeroSection.tsx
+++ src/app/(with-footer)/(home)/_sections/HeroSection/HeroSection.tsx
@@ -58,24 +58,34 @@ export const HeroSection = () => {
         </Button>
       </div>
       <div className={styles.actions}>
-        <Button className={styles.actionBtn} variant="outlined">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
-               stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-               className="icon icon-tabler icons-tabler-outline icon-tabler-file-download">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M14 3v4a1 1 0 0 0 1 1h4" />
-            <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
-            <path d="M12 17v-6" />
-            <path d="M9.5 14.5l2.5 2.5l2.5 -2.5" />
-          </svg>
-          <NextLink
-            href="/assets/files/MaximoComperatore-Resume.pdf"
-            target="_blank"
-            prefetch={true}
-          >
+        <NextLink
+          href="/assets/files/MaximoComperatore-Resume.pdf"
+          target="_blank"
+          prefetch={true}
+          passHref // Ensures proper anchor rendering
+        >
+          <Button className={styles.actionBtn} variant="outlined">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-file-download"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+              <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
+              <path d="M12 17v-6" />
+              <path d="M9.5 14.5l2.5 2.5l2.5 -2.5" />
+            </svg>
             {t("download")}
-          </NextLink>
-        </Button>
+          </Button>
+        </NextLink>
       </div>
       <CopyEmailSmallButton className={styles.copyEmailButton} />
       <Chatbot />
