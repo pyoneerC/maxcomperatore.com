ae20d2c8861b15ea238c8ee5c5ec3dd369b83be1
pyoneerC <maxcomperatutti@gmail.com>
2025-02-05 13:11:35 -0300

    fix: logo rearranging

--- src/data/skills.tsx
+++ src/data/skills.tsx
@@ -16,34 +16,6 @@ export const skills = [
 		colors: ["#eac347", "#FFE17D", "#caa30a", "#FFD700", "#FFDD44", "#F4C542", "#E6B800"]
 	},
 	{
-		name: "Docker",
-		icon: <DockerLogo />,
-		brandColor: "#00b8ff",
-		tooltip: "Deploys apps in isolated containers for consistent performance.",
-		colors: ["#099CEC", "#00ADEF", "#0077B5"],
-	},
-	{
-		name: "Azure",
-		icon: <AzureLogo />,
-		brandColor: "#12c0fd",
-		tooltip: "Microsoft’s cloud for scalable apps, AI, and data solutions.",
-		colors: ["#34BDEE", "#0078D4", "#00599C"],
-	},
-	{
-		name: "Supabase",
-		icon: <SupabaseLogo />,
-		brandColor: "#3ECF8E",
-		tooltip: "Open-source Firebase alternative for databases, auth, and storage.",
-		colors: ["#2F9E6C", "#3ECF8E", "#249D63"],
-	},
-	{
-		name: "DotNet",
-		icon: <DotNetLogo />,
-		brandColor: "#6540e8",
-		tooltip: "Framework for building scalable, enterprise-grade applications.",
-		colors: ["#512BD4", "#683AB7", "#3C1E70"],
-	},
-	{
 		name: "n8n",
 		icon: <N8nLogo />,
 		brandColor: "#EA4B71",
@@ -58,10 +30,38 @@ export const skills = [
 		colors: ["#0E7C63", "#15B091", "#087F6D"],
 	},
 	{
+		name: "Azure",
+		icon: <AzureLogo />,
+		brandColor: "#12c0fd",
+		tooltip: "Microsoft’s cloud for scalable apps, AI, and data solutions.",
+		colors: ["#34BDEE", "#0078D4", "#00599C"],
+	},
+	{
+		name: "DotNet",
+		icon: <DotNetLogo />,
+		brandColor: "#6540e8",
+		tooltip: "Framework for building scalable, enterprise-grade applications.",
+		colors: ["#512BD4", "#683AB7", "#3C1E70"],
+	},
+	{
+		name: "Docker",
+		icon: <DockerLogo />,
+		brandColor: "#00b8ff",
+		tooltip: "Deploys apps in isolated containers for consistent performance.",
+		colors: ["#099CEC", "#00ADEF", "#0077B5"],
+	},
+	{
 		name: "React",
 		icon: <ReactLogo />,
 		brandColor: "#11bfed",
 		tooltip: "Library for creating fast, interactive user interfaces for web apps.",
 		colors: ["#61DAFB", "#2196F3", "#0D47A1"],
+	},
+	{
+		name: "Supabase",
+		icon: <SupabaseLogo />,
+		brandColor: "#3ECF8E",
+		tooltip: "Open-source Firebase alternative for databases, auth, and storage.",
+		colors: ["#2F9E6C", "#3ECF8E", "#249D63"],
 	},
 ];
