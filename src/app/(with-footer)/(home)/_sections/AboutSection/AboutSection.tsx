6d610822e70a0552e2bd1e7aeba5f24862d7257a
pyoneerC <maxcomperatutti@gmail.com>
2025-03-12 19:02:05 -0300

    cal dot com

--- src/app/(with-footer)/(home)/_sections/AboutSection/AboutSection.tsx
+++ src/app/(with-footer)/(home)/_sections/AboutSection/AboutSection.tsx
@@ -1,16 +1,34 @@
-import React from 'react'
-import styles from './AboutSection.module.css'
-import {useTranslations} from "next-intl"
+"use client"
+
+import React, { useEffect } from "react";
+import styles from "./AboutSection.module.css";
+import { useTranslations } from "next-intl";
+import { getCalApi } from "@calcom/embed-react";
 import { Experience } from "~/components/Experience"
 
 export const AboutSection = () => {
-	const t = useTranslations("AboutSection")
+	const t = useTranslations("AboutSection");
+
+	useEffect(() => {
+		(async function () {
+			const cal = await getCalApi({ namespace: "30min" });
+			cal("floatingButton", {
+				calLink: "maximo-comperatore/30min",
+				config: { layout: "month_view" },
+				hideButtonIcon: false,
+				buttonPosition: "bottom-left",
+				buttonText:"Let's Talk"
+			});
+			cal("ui", {
+				hideEventTypeDetails: false,
+				layout: "month_view",
+			});
+		})();
+	}, []);
 
 	return (
 		<section className="section-wrapper">
-			<h2 className={styles.title}>
-				{t("title")}
-			</h2>
+			<h2 className={styles.title}>{t("title")}</h2>
 
 			<div className={styles.aboutMeContainer}>
 				<div className={styles.photoWrapper}>
@@ -22,41 +40,40 @@ export const AboutSection = () => {
 						width={300}
 						height={300}
 						style={{
-							objectFit: "cover", // Ensures the image fills the container without distortion
+							objectFit: "cover",
 						}}
 					/>
 				</div>
 				<div className={styles.textWrapper}>
 					<p className={styles.paragraph}>
-						{t.rich('d0', {
+						{t.rich("d0", {
 							strong: (children) => <strong>{children}</strong>,
-							em: (children) => <em>{children}</em>
+							em: (children) => <em>{children}</em>,
 						})}
 					</p>
 					<p className={styles.paragraph}>
-						{t.rich('d1', {
-							strong: (children) => <strong>{children}</strong>
+						{t.rich("d1", {
+							strong: (children) => <strong>{children}</strong>,
 						})}
 					</p>
 					<p className={styles.paragraph}>
-						{t.rich('d2', {
+						{t.rich("d2", {
 							strong: (children) => <strong>{children}</strong>,
-							em: (children) => <em>{children}</em>
+							em: (children) => <em>{children}</em>,
 						})}
 					</p>
 					<p className={styles.paragraph}>
-						{t.rich('d3', {
+						{t.rich("d3", {
 							strong: (children) => <strong>{children}</strong>,
-							em: (children) => <em>{children}</em>
+							em: (children) => <em>{children}</em>,
 						})}
 					</p>
 				</div>
 			</div>
-			<div
-				className={styles.experienceWrapper}>
+
+			<div className={styles.experienceWrapper}>
 				<Experience />
 			</div>
-
 		</section>
-	)
-}
+	);
+};
