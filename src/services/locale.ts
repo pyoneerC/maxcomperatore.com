6f440a63ea758dca0492566018e2828fdbd7125f
pyoneerC <maxcomperatutti@gmail.com>
2025-05-01 19:49:15 -0300

    gsap is now free

--- src/services/locale.ts
+++ src/services/locale.ts
@@ -8,7 +8,7 @@ import { Locale, defaultLocale } from "~/config"
 const COOKIE_NAME = "NEXT_LOCALE"
 
 export async function getUserLocale() {
-	return cookies().get(COOKIE_NAME)?.value || defaultLocale
+	return await cookies().get(COOKIE_NAME)?.value || defaultLocale
 }
 
 export async function setUserLocale(locale: Locale) {
