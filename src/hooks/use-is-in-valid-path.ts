1ed842257ec4a4c8c62eb2ab86dd622f7b95eb85
Pulpoide <joacolivero@gmail.com>
2025-06-17 20:25:09 -0300

    feat(hooks): reintroduce useIsValidAppRoute for route validation

---
+++ src/hooks/use-is-in-valid-path.ts
@@ -0,0 +1,8 @@
+import { usePathname } from "next/navigation"
+import { APP_ROUTES } from "~/constants"
+
+export const useIsValidAppRoute = () => {
+  const pathname = usePathname()
+
+  return APP_ROUTES.includes(pathname)
+}
