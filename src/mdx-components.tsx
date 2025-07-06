cbd112d70130fcc20cb225b5f486597b5d345a59
Pulpoide <joacolivero@gmail.com>
2025-07-06 18:06:57 -0300

    refactor(components): remove unused ArticleHeader component

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,11 +1,9 @@
 import type { MDXComponents } from "mdx/types"
-import { ArticleHeader } from "~/components/ArticleHeader"
 import { Button } from "~/components/Ui/Button"
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
 	return {
 		...components,
-		ArticleHeader,
 		Button,
 	}
 }
