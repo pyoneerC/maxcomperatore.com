ecf1c828fcf912889fd71fd2d2aea4cb207360e5
Pulpoide <joacolivero@gmail.com>
2025-07-19 00:19:07 -0300

    fix geist bug

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  transpilePackages: ['geist'],
 }
 
 const withMDX = createMDX({
