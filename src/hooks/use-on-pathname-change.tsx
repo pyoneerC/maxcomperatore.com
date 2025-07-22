599247687865d51f0e04f798774225976961309a
Pulpoide <joacolivero@gmail.com>
2025-07-22 02:38:49 -0300

    fix ESLint warning

--- src/hooks/use-on-pathname-change.tsx
+++ src/hooks/use-on-pathname-change.tsx
@@ -1,11 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 import { usePathname } from "next/navigation";
 
 export const useOnPathnameChange = (callback: () => void) => {
 	const pathname = usePathname();
+	const callbackRef = useRef(callback);
 
 	useEffect(() => {
-		callback();
+		callbackRef.current = callback;
+	}, [callback]);
+
+	useEffect(() => {
+		callbackRef.current();
 	}, [pathname]);
 };
