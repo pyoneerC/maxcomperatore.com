372cb9fd9d540713841ed087952e059600a986ae
Pulpoide <joacolivero@gmail.com>
2025-07-22 01:37:20 -0300

    chore(config): remove ‘geist’ from transpilePackages in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  transpilePackages: ['geist'],
 }
 
 const withMDX = createMDX({
